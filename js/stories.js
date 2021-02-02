1dfa680eb7bc27fd830ea623449c87a4a8867e0f
ayrlin.renata <ayrlin.renata@gmail.com>
2021-02-02 22:04:17 +0000

    stories collapse and link tech. added Afterlife

--- js/stories.js
+++ js/stories.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
 	loadTitle();
+	openAnchor();
 });
 
 function loadTitle() {
 	//Remove load screen
 	$('#load-screen').css("display","none");
+}
+
+function openAnchor() {
+	var hash = window.location.hash.substr(1);
+	var ele = document.getElementById(hash);
+	doUncollapse(ele);
 }
